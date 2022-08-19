04843e504d6909b9e4c39c3b016f7c484677567e
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2022-08-19 17:50:37 -0300

    Test new function

--- script3.0.1.js
+++ script3.0.1.js
@@ -297,3 +297,50 @@ $("#hiddenInput").keyup(function(e) {
        doSomethingWith(character);
     }
 });
+
+var canvas = document.getElementById('myCanvas'),
+    ctx = canvas.getContext('2d'),
+    font = '14px sans-serif',
+    hasInput = false;
+
+canvas.onclick = function(e) {
+    if (hasInput) return;
+    addInput(e.clientX, e.clientY);
+}
+
+//Function to dynamically add an input box: 
+function addInput(x, y) {
+
+    var input = document.createElement('input');
+
+    input.type = 'text';
+    input.style.position = 'fixed';
+    input.style.left = (x - 4) + 'px';
+    input.style.top = (y - 4) + 'px';
+
+    input.onkeydown = handleEnter;
+
+    document.body.appendChild(input);
+
+    input.focus();
+
+    hasInput = true;
+}
+
+//Key handler for input box:
+function handleEnter(e) {
+    var keyCode = e.keyCode;
+    if (keyCode === 13) {
+        drawText(this.value, parseInt(this.style.left, 10), parseInt(this.style.top, 10));
+        document.body.removeChild(this);
+        hasInput = false;
+    }
+}
+
+//Draw the text onto canvas:
+function drawText(txt, x, y) {
+    ctx.textBaseline = 'top';
+    ctx.textAlign = 'left';
+    ctx.font = font;
+    ctx.fillText(txt, x - 4, y - 4);
+}
